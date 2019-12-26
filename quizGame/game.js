e6ce37e2511fccb84b3d01a5e12aa9a1fd38ff4e
andrewswint <andrewswint1976@gmail.com>
2019-12-26 17:36:15 -0600

    keeping score and progress

--- quizGame/game.js
+++ quizGame/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score")
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -51,6 +53,8 @@ getNewQuestions = () => {
         return window.location.assign("/end.html");
     }
     questionCounter ++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -75,6 +79,10 @@ choices.forEach(choice => {
             classToApply = "correct";
         }
 
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
@@ -83,6 +91,12 @@ choices.forEach(choice => {
         }, 1000);
     });
 });
+
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
+
 
 
 startGame();
