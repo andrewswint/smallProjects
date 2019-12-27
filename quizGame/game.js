f93f132e949209f547866d95ac263a462aedd3ce
andrewswint <andrewswint1976@gmail.com>
2019-12-26 20:55:56 -0600

    setting up end screen and local storage

--- quizGame/game.js
+++ quizGame/game.js
@@ -50,7 +50,8 @@ startGame  = () => {
 getNewQuestions = () => {
 
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        return window.location.assign("/end.html");
+        localStorage.setItem("mostRecentScore", score);
+        return window.location.assign("end.html");
     }
     questionCounter ++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
