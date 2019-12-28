4d2a55ffc32f8b8f4132d88bc3eb33eafc85e07e
andrewswint <andrewswint1976@gmail.com>
2019-12-27 18:18:23 -0600

    adding to list via javascript

---
+++ 12DaysOfCode/js/dec27.js
@@ -0,0 +1,9 @@
+let input = document.getElementById("form1");
+let url = document.getElementById("urlInput")
+
+
+addSong = () => {
+    input.value;
+    document.getElementById("list").innerHTML =
+        `<li>${input}</li>`
+};
