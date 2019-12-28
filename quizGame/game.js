6c94cf6bc971e19fbca3ef53158aace2e0e667ad
andrewswint <andrewswint1976@gmail.com>
2019-12-27 20:14:55 -0600

    open api for questions through fetch

--- quizGame/game.js
+++ quizGame/game.js
@@ -9,14 +9,45 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+
+//===================Questions from json
+// let questions = [];
+// fetch("questions.json")
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((loadedQuestions) => {
+//         questions = loadedQuestions;
+//     startGame();
+//     })
+//     .catch((err) => {
+//         console.log("error")
+//     });
+
+//===================Questions from opentdb.com api
 let questions = [];
-fetch("questions.json")
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
     .then((res) => {
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions;
-    startGame();
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+            return formattedQuestion;
+        });
+        startGame();
     })
     .catch((err) => {
         console.log("error")
