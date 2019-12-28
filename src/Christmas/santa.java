1e72da4d108ed1cc4225c8aed2044da90ddac199
andrewswint <andrewswint1976@gmail.com>
2019-12-28 10:19:07 -0600

    refactor to import util class

--- src/Christmas/santa.java
+++ src/Christmas/santa.java
@@ -7,10 +7,11 @@ public class santa {
 
 
     public static void santasQuestions () {
-        Scanner sc = new Scanner(System.in);
+        Input input = new Input();
         System.out.println("What do you want for Christmas?");
-        String userInput = sc.nextLine();
+        String userInput = input.getString();
         if (userInput.length() == 0) {
+            System.out.println("Please enter a valid response...");
             santasQuestions();
         }
         System.out.println(userInput);
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,12 +4,9 @@ public class InputTest {
     public static void main(String[] args) throws Exception {
         Input input = new Input();
 //        System.out.println(input.yesNo());
-//        input.getInt(1,10);
-//        input.getInt();
-//        input.getDouble(1, 2);
-//        input.getDouble();
-        input.getBinary();
-        input.getHex();
+
+        input.getInt();
+
 
     }
 }
