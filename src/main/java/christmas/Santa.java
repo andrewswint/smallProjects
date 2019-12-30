d02f86a6dafb7d1698eb89d241696ac57f19d063
andrewswint <andrewswint1976@gmail.com>
2019-12-30 09:13:58 -0600

    array created

--- src/main/java/christmas/Santa.java
+++ src/main/java/christmas/Santa.java
@@ -1,9 +1,11 @@
 package christmas;
 import util.Input;
 
+import java.util.Arrays;
+
 public class Santa {
 
-
+    public static Reindeer[] reindeer = new Reindeer[9];
 
     public static void santasQuestions () {
         Input input = new Input();
@@ -16,12 +18,38 @@ public class Santa {
         System.out.println(userInput);
     }
 
+
+
     public static void main(String[] args) {
 
-        santasQuestions();
+//        santasQuestions();
 
         Reindeer donner = new Reindeer("donner");
+        reindeer[0] = donner;
 
+        Reindeer donner2 = new Reindeer("donner2");
+        reindeer[1] = donner2;
+
+        Reindeer donner3 = new Reindeer("donner3");
+        reindeer[2] = donner3;
+
+        Reindeer donner4 = new Reindeer("donner4");
+        reindeer[3] = donner4;
+
+        Reindeer donner5 = new Reindeer("donner5");
+        reindeer[4] = donner5;
+
+        Reindeer donner6 = new Reindeer("donner6");
+        reindeer[5] = donner6;
+
+        Reindeer donner7 = new Reindeer("donner7");
+        reindeer[6] = donner7;
+
+        Reindeer donner8 = new Reindeer("donner8");
+        reindeer[7] = donner8;
+
+        Reindeer donner9 = new Reindeer("donner9");
+        reindeer[8] = donner9;
 
     }
 }
