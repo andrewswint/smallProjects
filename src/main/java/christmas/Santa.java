afdc7e5ee138fe122075d2fba460fb599e8d1579
andrewswint <andrewswint1976@gmail.com>
2020-01-01 11:08:09 -0600

    adding arraylist for wishlist

--- src/main/java/christmas/Santa.java
+++ src/main/java/christmas/Santa.java
@@ -1,11 +1,13 @@
 package christmas;
 import util.Input;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Santa {
 
     public static Reindeer[] reindeer = new Reindeer[9];
+    static ArrayList<String> wishList = new ArrayList<>();
 
     public static void santasQuestions () {
         Input input = new Input();
@@ -15,7 +17,8 @@ public class Santa {
             System.out.println("Please enter a valid response...");
             santasQuestions();
         }
-        System.out.println(userInput);
+        wishList.add(userInput);
+        System.out.println(wishList);
     }
 
 
@@ -29,41 +32,42 @@ public class Santa {
 
     public static void main(String[] args) {
 
-//        santasQuestions();
+        santasQuestions();
 
-        Reindeer donner = new Reindeer("donner");
-        reindeer[0] = donner;
 
-        Reindeer donner2 = new Reindeer("donner2");
-        reindeer[1] = donner2;
-
-        Reindeer donner3 = new Reindeer("donner3");
-        reindeer[2] = donner3;
-
-        Reindeer donner4 = new Reindeer("donner4");
-        reindeer[3] = donner4;
-
-        Reindeer donner5 = new Reindeer("donner5");
-        reindeer[4] = donner5;
-
-        Reindeer donner6 = new Reindeer("donner6");
-        reindeer[5] = donner6;
-
-        Reindeer donner7 = new Reindeer("donner7");
-        reindeer[6] = donner7;
-
-        Reindeer donner8 = new Reindeer("donner8");
-        reindeer[7] = donner8;
-
-        Reindeer donner9 = new Reindeer("donner9");
-        reindeer[8] = donner9;
-
-        Reindeer donner10 = new Reindeer("donner10");
-
-        reindeer = addReindeer(reindeer, donner10);
-
-        for (int i = 0; i < reindeer.length; i++) {
-            System.out.println(reindeer[i].getName());
-        }
+//        Reindeer donner = new Reindeer("donner");
+//        reindeer[0] = donner;
+//
+//        Reindeer donner2 = new Reindeer("donner2");
+//        reindeer[1] = donner2;
+//
+//        Reindeer donner3 = new Reindeer("donner3");
+//        reindeer[2] = donner3;
+//
+//        Reindeer donner4 = new Reindeer("donner4");
+//        reindeer[3] = donner4;
+//
+//        Reindeer donner5 = new Reindeer("donner5");
+//        reindeer[4] = donner5;
+//
+//        Reindeer donner6 = new Reindeer("donner6");
+//        reindeer[5] = donner6;
+//
+//        Reindeer donner7 = new Reindeer("donner7");
+//        reindeer[6] = donner7;
+//
+//        Reindeer donner8 = new Reindeer("donner8");
+//        reindeer[7] = donner8;
+//
+//        Reindeer donner9 = new Reindeer("donner9");
+//        reindeer[8] = donner9;
+//
+//        Reindeer donner10 = new Reindeer("donner10");
+//
+//        reindeer = addReindeer(reindeer, donner10);
+//
+//        for (int i = 0; i < reindeer.length; i++) {
+//            System.out.println(reindeer[i].getName());
+//        }
     }
 }
