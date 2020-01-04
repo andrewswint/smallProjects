44521b86543662da80b2b55ff7100c76bacd833f
andrewswint <andrewswint1976@gmail.com>
2020-01-03 19:33:59 -0600

    working on seeder

--- santasDb.sql
+++ santasDb.sql
@@ -1,3 +1,18 @@
-CREATE USER 'santaclaus'@'localhost' IDENTIFIED BY 'password';
+# CREATE USER 'santaclaus'@'localhost' IDENTIFIED BY 'password';
+#
+# GRANT ALL ON *.* TO 'santaclaus'@'localhost';
 
-GRANT ALL ON *.* TO 'santaclaus'@'localhost';
+CREATE DATABASE IF NOT EXISTS 'christmas_db';
+
+USE 'christmas_db';
+
+CREATE TABLE people(
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    birthday VARCHAR(50) NOT NULL,
+    nice BOOLEAN NOT NULL,
+    wishlist BLOB
+);
+
+INSERT INTO people
