d22bdbcf76f3005b8fe46a04ab9eed9e677d4246
andrewswint <andrewswint1976@gmail.com>
2020-01-05 15:09:07 -0600

    delete functionality for santa exercise

--- santasDb.sql
+++ santasDb.sql
@@ -17,7 +17,9 @@ USE christmas_db;
 # ALTER TABLE people
 #     ADD  UNIQUE (first_name, last_name);
 
-INSERT INTO people (first_name, last_name, age, birthday, nice, wishlist)
-    VALUES ('James', 'Swint', 1, 'aug 1 2020', true, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something'),
-           ('Lillian', 'Swint', 1, 'aug 2 2020', true, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something'),
-           ('Joan', 'Swint', 1, 'aug 3 2020', false, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something');
+# INSERT INTO people (first_name, last_name, age, birthday, nice, wishlist)
+#     VALUES ('James', 'Swint', 1, 'aug 1 2020', true, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something'),
+#            ('Lillian', 'Swint', 1, 'aug 2 2020', true, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something'),
+#            ('Joan', 'Swint', 1, 'aug 3 2020', false, 'thing 1, thing 2, thing 3, thing 4, thing 5, thing 6, something else, another something, some more something');
+
+DELETE FROM people WHERE nice = false;
