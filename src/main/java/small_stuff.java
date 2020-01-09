b356d4712c481a0b8859ce6b010f884d0c5e4390
andrewswint <andrewswint1976@gmail.com>
2020-01-09 09:34:11 -0600

    morning warmup

--- src/main/java/small_stuff.java
+++ src/main/java/small_stuff.java
@@ -13,18 +13,29 @@ public class small_stuff {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+//    morning warmup. take in a number and add every number starting at 1 going up to the established number
+    public static int addingUp (int num) {
+        int bucket = 0;
+        for (int i = 0; i <= num; i++) {
+            bucket += i;
+        }
+        return bucket;
+    }
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        scan = new Scanner(System.in);
-        double d = scan.nextDouble();
-        scan = new Scanner(System.in);
-        String s = scan.next();
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+//        int i = scan.nextInt();
+//        scan = new Scanner(System.in);
+//        double d = scan.nextDouble();
+//        scan = new Scanner(System.in);
+//        String s = scan.next();
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
+        int num = scan.nextInt();
+        System.out.println(addingUp(num));
 
     }
 
