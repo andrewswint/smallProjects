260dc4b1e229a17e1ecad18fceebabca355bc3eb
andrewswint <andrewswint1976@gmail.com>
2020-01-11 18:18:28 -0600

    variables established, starting constructor, getters and setters for objejcts

--- src/main/java/emailApp/Email.java
+++ src/main/java/emailApp/Email.java
@@ -2,6 +2,11 @@ package emailApp;
 
 public class Email {
 
-
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 
 }
