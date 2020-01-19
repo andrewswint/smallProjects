b280e95872a2fa05b01ff471eebd994acd2cdc75
andrewswint <andrewswint1976@gmail.com>
2020-01-18 18:29:05 -0600

    testing string builder for adlister project categories

--- src/main/java/exercises.java
+++ src/main/java/exercises.java
@@ -46,7 +46,21 @@ public class exercises {
 
     public static void main(String[] args) throws IOException {
 
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list)
+        {
+            sb.append(s);
+            sb.append("\t");
         }
+
+        System.out.println(sb.toString());
+
+    }
     }
 
 
