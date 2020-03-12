7282b3654a6904485ae500901c77cca38cf8946b
andrewswint <andrewswint1976@gmail.com>
2020-03-11 19:26:08 -0500

    learning about testing arrays in junit

--- src/test/java/practiceTests.java
+++ src/test/java/practiceTests.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import exercises.*;
@@ -5,9 +6,18 @@ import exercises.*;
 public class practiceTests {
 
     @Test
-    public void perimeterTest (){
+    public void perimeterTest(){
         assertEquals(14, edabit.perimeter(3,4));
         assertEquals(0, edabit.perimeter(0,5));
+
+    }
+
+    @Test
+    public void denimFlipTest() {
+        int[] arr ={1,2,3,4};
+        int[] arr2 = new int[0];
+        Assert.assertArrayEquals(new int[]{4,3,2,1}, denimFlip.flip(arr));
+        Assert.assertArrayEquals(new int[]{}, denimFlip.flip(arr2));
 
     }
 
