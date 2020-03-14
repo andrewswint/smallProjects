bedeef02031f0e2228b9b22d499a3adbec2614a3
andrewswint <andrewswint1976@gmail.com>
2020-03-14 18:30:02 -0500

    int convertion hackranker exercise

--- src/main/java/exercises/edabit.java
+++ src/main/java/exercises/edabit.java
@@ -1,30 +1,31 @@
 package exercises;
 import java.io.*;
 import java.util.*;
+import java.security.*;
 
 
 public class edabit {
 
-    static int B;
-    static int H;
-    private static boolean flag = false;
-
-    static {
-        Scanner sc = new Scanner(System.in);
-        B = sc.nextInt();
-        H = sc.nextInt();
-        sc.close();
-        if (B > 0 && H > 0) {
-            flag = true;
-        } else {
-            try {
-                System.out.println("java.lang.Exception: Breadth and height must be positive");
-                throw new Exception("Breadth and height must be positive");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    static int B;
+//    static int H;
+//    private static boolean flag = false;
+//
+//    static {
+//        Scanner sc = new Scanner(System.in);
+//        B = sc.nextInt();
+//        H = sc.nextInt();
+//        sc.close();
+//        if (B > 0 && H > 0) {
+//            flag = true;
+//        } else {
+//            try {
+//                System.out.println("java.lang.Exception: Breadth and height must be positive");
+//                throw new Exception("Breadth and height must be positive");
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
 //    public static String name(String first, String last) {
 //        return first + " " + last;
@@ -39,13 +40,26 @@ public class edabit {
 //    }
 
 
+
+
+
     public static void main(String[] args){
-        if(flag){
-            int area=B*H;
-            System.out.print(area);
-        }
+//        if(flag){
+//            int area=B*H;
+//            System.out.print(area);
+//        }
 
+            Scanner in = new Scanner(System.in);
+            int n = in .nextInt();
+            in.close();
+            String s = String.valueOf(n);
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
     }
-
 }
+
 
