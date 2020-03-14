09d0ccc6e31b8b08e03b55f305aa2977c164b65a
andrewswint <andrewswint1976@gmail.com>
2020-03-14 16:39:06 -0500

    static initialization block and exception throw exercise

--- src/main/java/exercises/edabit.java
+++ src/main/java/exercises/edabit.java
@@ -5,29 +5,47 @@ import java.util.*;
 
 public class edabit {
 
-    public static int perimeter (int height, int width) {
-        if (height == 0 || width == 0) {
-            return 0;
-        }
-        return (2 * (height + width));
-    }
+    static int B;
+    static int H;
+    private static boolean flag = false;
 
-    public static String name(String first, String last) {
-        return first + " " + last;
-    }
-
-    public static int parallelogram(int b, int h) throws Exception {
-        if (b > 0 && h > 0) {
-            return b * h;
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        sc.close();
+        if (B > 0 && H > 0) {
+            flag = true;
         } else {
-            throw new Exception("Breadth and height must be positive");
+            try {
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+                throw new Exception("Breadth and height must be positive");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
+//    public static String name(String first, String last) {
+//        return first + " " + last;
+//    }
+//
+//    public static int parallelogram(int B, int H) throws Exception {
+//        if (B > 0 && H > 0) {
+//            return B * H;
+//        } else {
+//            throw new Exception("Breadth and height must be positive");
+//        }
+//    }
 
-    public static void main(String[] args) throws Exception {
-//        Scanner sc = new Scanner(System.in);
-        parallelogram(-1,2);
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
     }
+
 }
 
--- src/test/java/practiceTests.java
+++ src/test/java/practiceTests.java
@@ -11,48 +11,42 @@ import java.util.Scanner;
 
 public class practiceTests<TextFromStandardInputStream> {
 
-    @Test
-    public void perimeterTest(){
-        assertEquals(14, edabit.perimeter(3,4));
-        assertEquals(0, edabit.perimeter(0,5));
 
-    }
-
-    @Test
-    public void denimFlipTest() {
-        int[] arr ={1,2,3,4};
-        int[] arr2 = new int[0];
-        Assert.assertArrayEquals(new int[]{4,3,2,1}, denimFlip.flip(arr));
-        Assert.assertArrayEquals(new int[]{}, denimFlip.flip(arr2));
-
-    }
-
-    @Test
-    public void nameConcatTest() {
-        assertEquals("Andrew Swint", edabit.name("Andrew", "Swint"));
-    }
-
-    @Test
-    public void EOFTest() {
-        String data = "line 1\n" +
-                "line 2\n" +
-                "line 3\n";
-        InputStream stdin = System.in;
-        String output = "";
-        try {
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            Scanner sc = new Scanner(System.in);
-            int i = 0;
-            while(sc.hasNext()) {
-                String input = sc.nextLine();
-                i++;
-                output += (i + " " + input + "\n");
-            }
-        } finally {
-            System.setIn(stdin);
-        }
-        assertEquals("1 line 1\n" +
-                "2 line 2\n" +
-                "3 line 3\n", output);
-    }
+//    @Test
+//    public void denimFlipTest() {
+//        int[] arr ={1,2,3,4};
+//        int[] arr2 = new int[0];
+//        Assert.assertArrayEquals(new int[]{4,3,2,1}, denimFlip.flip(arr));
+//        Assert.assertArrayEquals(new int[]{}, denimFlip.flip(arr2));
+//
+//    }
+//
+//    @Test
+//    public void nameConcatTest() {
+//        assertEquals("Andrew Swint", edabit.name("Andrew", "Swint"));
+//    }
+//
+//    @Test
+//    public void EOFTest() {
+//        String data = "line 1\n" +
+//                "line 2\n" +
+//                "line 3\n";
+//        InputStream stdin = System.in;
+//        String output = "";
+//        try {
+//            System.setIn(new ByteArrayInputStream(data.getBytes()));
+//            Scanner sc = new Scanner(System.in);
+//            int i = 0;
+//            while(sc.hasNext()) {
+//                String input = sc.nextLine();
+//                i++;
+//                output += (i + " " + input + "\n");
+//            }
+//        } finally {
+//            System.setIn(stdin);
+//        }
+//        assertEquals("1 line 1\n" +
+//                "2 line 2\n" +
+//                "3 line 3\n", output);
+//    }
 }
