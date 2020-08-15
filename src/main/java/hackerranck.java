994dd49b26de73c5e86831d11ad2b35a054834a5
andrewswint <andrewswint1976@gmail.com>
2020-08-15 10:43:21 -0500

    problem solving practice. Want to get back to doing some of these daily

--- src/main/java/hackerranck.java
+++ src/main/java/hackerranck.java
@@ -14,13 +14,29 @@ public class hackerranck {
         return a + b;
     }
 
+    public static void checkType () {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println("* byte");
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) System.out.println("* short");
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) System.out.println("* int");
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
+        sc.close();
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(minSec(5));
-        System.out.println(minSec(3));
-        System.out.println(minSec(2));
-
-        System.out.println(convert(1,3));
+        checkType();
 
     }
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,9 @@
+import java.util.*;
+import java.io.*;
+
 public class main {
 
-    public static void main (String[] args) {
-
-
+    public static void main(String[] args) {
 
     }
 }
