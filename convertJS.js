aca7adf71a40e87f775d4a4a8126e1eeaf6e1bef
andrewswint <andrewswint1976@gmail.com>
2020-08-23 20:55:49 -0500

    trying to improve

--- convertJS.js
+++ convertJS.js
@@ -1,6 +1,7 @@
 function convert() {
     var output = document.getElementById("ti2");
     var input = document.getElementById("ti1").value;
+    // var output = document.getElementById("ti2").innerHTML
     output.value = "";
     for (var i = 0; i < input.length; i++) {
         output.value += input[i].charCodeAt(0).toString(2) + " ";
